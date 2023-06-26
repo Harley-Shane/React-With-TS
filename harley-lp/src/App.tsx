728d8eb518b7ac74cd3a0b667eedd82bdbb33c6a
Harley-Shane <harley.shane@outlook.com>
2023-06-26 18:23:19 -0400

    first commit

---
+++ harley-lp/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span className="heading">Harley Collins</span>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
